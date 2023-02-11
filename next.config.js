b9de7a2898b68c816fd7e62a20b5262cee8273a7
digitsense <moon@digit-sense.com>
2023-02-12 06:40:12 +0700

    change favicon

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	withCSS: {
-		cssModule: true,
-	},
 	images: {
 		domains: ['firebasestorage.googleapis.com'],
 	},
